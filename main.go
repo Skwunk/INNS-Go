370457a7ee88deaaaee3b19c3209d73b81f62bb0
Skwunk <jordan.cameron.vigo@gmail.com>
2021-04-20 13:11:51 +0100

    Improvement on plotting data

--- main.go
+++ main.go
@@ -17,12 +17,12 @@ import (
 
 type dataPoint struct {
 	Date                string
-	RentedBikes         int
-	Hour                int
+	RentedBikes         float64
+	Hour                float64
 	Temperature         float64
-	Humidity            int
+	Humidity            float64
 	WindSpeed           float64
-	Visibility          int
+	Visibility          float64
 	DewPointTemperature float64
 	SolarRadiation      float64
 	Rainfall            float64
@@ -56,12 +56,12 @@ func loadData() dataSet {
 		if line[13] == "No" {
 			continue
 		}
-		rentedBikes, _ := strconv.Atoi(line[1])
-		hour, _ := strconv.Atoi(line[2])
+		rentedBikes, _ := strconv.ParseFloat(line[1], 64)
+		hour, _ := strconv.ParseFloat(line[2], 64)
 		temperature, _ := strconv.ParseFloat(line[3], 64)
-		humidity, _ := strconv.Atoi(line[4])
+		humidity, _ := strconv.ParseFloat(line[4], 64)
 		windSpeed, _ := strconv.ParseFloat(line[5], 64)
-		visibility, _ := strconv.Atoi(line[6])
+		visibility, _ := strconv.ParseFloat(line[6], 64)
 		dewPointTemperature, _ := strconv.ParseFloat(line[7], 64)
 		solarRadiation, _ := strconv.ParseFloat(line[8], 64)
 		rainfall, _ := strconv.ParseFloat(line[9], 64)
@@ -91,17 +91,59 @@ func loadData() dataSet {
 }
 
 func plotData(data dataSet) {
+
+	// Sort data into better formats for plots
+	rentedBikesSlice := make([]float64, 64)
+	hourSlice := make([]float64, 64)
+	temperatureSlice := make([]float64, 64)
+	humiditySlice := make([]float64, 64)
+	windSpeedSlice := make([]float64, 64)
+	visibilitySlice := make([]float64, 64)
+	dewPointTemperatureSlice := make([]float64, 64)
+	solarRadiationSlice := make([]float64, 64)
+	rainfallSlice := make([]float64, 64)
+	snowfallSlice := make([]float64, 64)
+	for _, dataPoint := range data {
+		rentedBikesSlice = append(rentedBikesSlice, dataPoint.RentedBikes)
+		hourSlice = append(hourSlice, dataPoint.Hour)
+		temperatureSlice = append(temperatureSlice, dataPoint.Temperature)
+		humiditySlice = append(humiditySlice, dataPoint.Humidity)
+		windSpeedSlice = append(windSpeedSlice, dataPoint.WindSpeed)
+		visibilitySlice = append(visibilitySlice, dataPoint.Visibility)
+		dewPointTemperatureSlice = append(dewPointTemperatureSlice, dataPoint.DewPointTemperature)
+		solarRadiationSlice = append(solarRadiationSlice, dataPoint.SolarRadiation)
+		rainfallSlice = append(rainfallSlice, dataPoint.Rainfall)
+		snowfallSlice = append(snowfallSlice, dataPoint.Snowfall)
+	}
+	Slices := [][]float64{rentedBikesSlice, hourSlice, temperatureSlice, humiditySlice, windSpeedSlice, visibilitySlice,
+		dewPointTemperatureSlice, solarRadiationSlice, rainfallSlice, snowfallSlice}
+
+	// For each column of data, plot against another column of data
+	scatterData := make(plotter.XYs, n)
+	for indexA, sliceA := range Slices {
+		var n = len(sliceA)
+		for indexB, sliceB := range Slices {
+			if indexA != indexB {
+				for _, x := range sliceA {
+					for _, y := range sliceB {
+
+					}
+				}
+			}
+		}
+	}
+
+	//========================================
 	p := plot.New()
 	p.X.Label.Text = "Rented Bikes"
-	p.Y.Label.Text = "Wind Speed"
+	p.Y.Label.Text = "Temperature"
 	p.Add(plotter.NewGrid())
 
-	var n = len(data)
-	log.Println(n)
-	scatterData := make(plotter.XYs, n)
+	n = len(data)
+	scatterData = make(plotter.XYs, n)
 	for i := range scatterData {
-		scatterData[i].X = float64(data[i].RentedBikes)
-		scatterData[i].Y = float64(data[i].WindSpeed)
+		scatterData[i].X = float64(rentedBikesSlice[i])
+		scatterData[i].Y = float64(temperatureSlice[i])
 	}
 
 	s, err := plotter.NewScatter(scatterData)
@@ -113,7 +155,7 @@ func plotData(data dataSet) {
 	p.Add(s)
 	p.Legend.Add("scatter", s)
 
-	if err := p.Save(4*vg.Inch, 4*vg.Inch, "plot.png"); err != nil {
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "plots/plot.png"); err != nil {
 		log.Println(err)
 	}
 }
